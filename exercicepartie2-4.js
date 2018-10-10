96937ee0e28380af6e6d7c4fcd6782c971e5a2a7
JonathanLebourg <jonjonlebourg@gmail.com>
2018-10-10 13:28:50 +0200

    first commit

---
+++ exercicepartie2-4.js
@@ -0,0 +1,17 @@
+document.getElementsByName("button")[0].addEventListener("click", function(){
+  document.getElementById("lastname").value ="";
+  document.getElementById("firstname").value ="";
+  document.getElementById("city").value ="";
+  })
+
+// var button= document.getElementsByName("button")[0];
+// var lastname= document.getElementsByName("lastname");
+// var firstname= document.getElementsByName("firstname");
+// var city= document.getElementsByName("city");
+//
+// button.addEventListener("click", function(){
+//   lastname.value ="";
+//   firstname.value ="";
+//   city.value ="";
+// })
+// ge
